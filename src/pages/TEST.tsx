a3b518df2827409fd35bf2206aa0797409e55528
hoonshi <qkekxovnd@hufs-gsuite.kr>
2025-04-07 17:38:39 +0900

    feat: breadcrumb-depth를 나타내는 헤더 서브컴포넌트 제작-1

--- src/pages/TEST.tsx
+++ src/pages/TEST.tsx
@@ -1,3 +1,17 @@
+import Breadcrumb from '@/shared/ui/BreadCrumb';
+
 export default function TEST() {
-  return <div></div>;
+  const data = {
+    breadcrumb: [
+      { id: '1', name: '디렉토리1' },
+      { id: '2', name: '디렉토리2' },
+    ],
+    current: { id: '2', name: '디렉토리2' },
+  };
+
+  return (
+    <div>
+      <Breadcrumb items={data.breadcrumb} />
+    </div>
+  );
 }
