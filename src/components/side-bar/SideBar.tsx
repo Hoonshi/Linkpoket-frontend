062620a85de83c6d339c91bc8c4760fddd680da2
hoonshi <qkekxovnd@hufs-gsuite.kr>
2025-07-13 04:23:32 +0900

    feat: 반응형 임시 처리

--- src/components/side-bar/SideBar.tsx
+++ src/components/side-bar/SideBar.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { Link } from 'react-router-dom';
 import BookMark from '@/assets/widget-ui-assets/BookMark.svg?react';
 import PersonalPage from '@/assets/widget-ui-assets/PersonalPage.svg?react';
@@ -15,9 +15,16 @@ import { usePageStore, useParentsFolderIdStore } from '@/stores/pageStore';
 type MenubarProps = {
   showSidebar: boolean;
   setShowSidebar: React.Dispatch<React.SetStateAction<boolean>>;
+  isFoldSidebar: boolean;
+  setIsFoldSidebar: React.Dispatch<React.SetStateAction<boolean>>;
 };
 
-const SideBar: React.FC<MenubarProps> = ({ showSidebar, setShowSidebar }) => {
+const SideBar: React.FC<MenubarProps> = ({
+  showSidebar,
+  setShowSidebar,
+  isFoldSidebar,
+  setIsFoldSidebar,
+}) => {
   const sidebarRef = useRef<HTMLElement | null>(null);
   const isMobile = useMobile();
   const { pageId } = usePageStore();
@@ -88,125 +95,132 @@ const SideBar: React.FC<MenubarProps> = ({ showSidebar, setShowSidebar }) => {
     });
   };
 
-  return showSidebar ? (
-    <aside
-      ref={sidebarRef}
-      className={`border-gray-10 flex h-screen w-[220px] flex-col justify-between border-r ${isMobile ? 'bg-gray-0 absolute top-0 left-0 z-50' : 'relative'} `}
-    >
-      <div className="flex flex-col gap-[8px] p-[16px]">
-        <div className="flex justify-end">
-          <button
-            onClick={() => setShowSidebar(false)}
-            className="cursor-pointer"
-          >
-            <SidebarClose />
-          </button>
-        </div>
-        <ul>
-          <li>
-            <Link
-              to="/"
-              className="group hover:bg-primary-5 text-gray-70 focus:bg-primary-10 focus:text-primary-50 flex items-center gap-[12px] p-[8px] text-[14px] font-[600] hover:rounded-[8px] focus:rounded-[8px]"
+  return (
+    showSidebar && (
+      <aside
+        ref={sidebarRef}
+        className={`border-gray-10 flex h-screen w-[220px] flex-col justify-between border-r ${isMobile ? 'bg-gray-0 absolute top-0 left-0 z-50' : 'relative'} `}
+      >
+        <div className="flex flex-col gap-[8px] p-[16px]">
+          <div className="flex justify-end">
+            <button
+              onClick={() => {
+                setShowSidebar(false);
+                setIsFoldSidebar(true);
+              }}
+              className="cursor-pointer"
             >
-              <PersonalPage
-                width={20}
-                height={20}
-                className="group-focus:text-primary-50 text-gray-70"
-              />
-              개인 페이지
-            </Link>
-
-            <Link
-              to="bookmarks"
-              className="hover:bg-primary-5 group text-gray-70 focus:bg-primary-10 focus:text-primary-50 flex items-center gap-[12px] p-[8px] text-[14px] font-[600] hover:rounded-[8px] focus:rounded-[8px]"
-            >
-              <BookMark
-                width={20}
-                height={20}
-                className="text-gray-70 group-focus:text-primary-50 my-[2px]"
-              />
-              북마크
-            </Link>
-
-            <div className="mt-4 flex items-center px-[8px] py-[4px] text-[14px] font-[500] text-gray-50 hover:rounded-[8px] focus:rounded-[8px]">
-              <div className="group flex w-full items-center justify-between">
-                <div className="flex gap-[20px]">
-                  <div>공유 페이지</div>
-                </div>
-                <PlusIcon
-                  className="text-gray-40 hover:text-gray-90 cursor-pointer"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    e.preventDefault();
-                    handleCreateSharedPage();
-                  }}
+              <SidebarClose />
+            </button>
+          </div>
+          <ul>
+            <li>
+              <Link
+                to="/"
+                className="group hover:bg-primary-5 text-gray-70 focus:bg-primary-10 focus:text-primary-50 flex items-center gap-[12px] p-[8px] text-[14px] font-[600] hover:rounded-[8px] focus:rounded-[8px]"
+              >
+                <PersonalPage
+                  width={20}
+                  height={20}
+                  className="group-focus:text-primary-50 text-gray-70"
                 />
-              </div>
-            </div>
+                개인 페이지
+              </Link>
 
-            {/* 공유페이지 리스트 */}
-            <div className="mt-2 flex flex-col gap-[2px]">
-              {joinedPage?.map((page: any) => (
-                <Link
-                  key={page.pageId}
-                  to={`/shared/${page.pageId}`}
-                  className="text-gray-70 hover:bg-gray-5 focus:bg-gray-5 py-2 pr-3 pl-2 text-[14px] font-[600] hover:rounded-[8px] focus:rounded-[8px]"
-                >
-                  {page.pageTitle}
-                </Link>
-              ))}
-            </div>
-
-            {/* 공유페이지에 따른 폴더 생성  */}
-            <div className="mt-4 flex items-center px-[8px] py-[4px] text-[14px] font-[500] text-gray-50 hover:rounded-[8px] focus:rounded-[8px]">
-              <div className="group flex w-full items-center justify-between">
-                <div className="flex gap-[20px]">
-                  <div>폴더</div>
-                </div>
-                <PlusIcon
-                  className="text-gray-40 hover:text-gray-90 cursor-pointer"
-                  onClick={(e) => {
-                    e.stopPropagation();
-                    e.preventDefault();
-                    handleCreateFolder();
-                  }}
+              <Link
+                to="bookmarks"
+                className="hover:bg-primary-5 group text-gray-70 focus:bg-primary-10 focus:text-primary-50 flex items-center gap-[12px] p-[8px] text-[14px] font-[600] hover:rounded-[8px] focus:rounded-[8px]"
+              >
+                <BookMark
+                  width={20}
+                  height={20}
+                  className="text-gray-70 group-focus:text-primary-50 my-[2px]"
                 />
-              </div>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </aside>
-  ) : (
-    // 반응형 (사이드바 접힘) 화면
-    <aside className="border-gray-10 h-screen w-[80px] border-r p-4">
-      <div className="flex justify-end">
-        <button
-          onClick={() => setShowSidebar(true)}
-          className="mb-2 cursor-pointer"
-        >
-          <SidebarOpen />
-        </button>
-      </div>
+                북마크
+              </Link>
 
-      <div className="flex flex-col items-center gap-[8px]">
-        <div className="p-3">
-          <PersonalPage
-            width={20}
-            height={20}
-            className="group-focus:text-primary-50 text-gray-70"
-          />
+              <div className="mt-4 flex items-center px-[8px] py-[4px] text-[14px] font-[500] text-gray-50 hover:rounded-[8px] focus:rounded-[8px]">
+                <div className="group flex w-full items-center justify-between">
+                  <div className="flex gap-[20px]">
+                    <div>공유 페이지</div>
+                  </div>
+                  <PlusIcon
+                    className="text-gray-40 hover:text-gray-90 cursor-pointer"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      e.preventDefault();
+                      handleCreateSharedPage();
+                    }}
+                  />
+                </div>
+              </div>
+
+              {/* 공유페이지 리스트 */}
+              <div className="mt-2 flex flex-col gap-[2px]">
+                {joinedPage?.map((page: any) => (
+                  <Link
+                    key={page.pageId}
+                    to={`/shared/${page.pageId}`}
+                    className="text-gray-70 hover:bg-gray-5 focus:bg-gray-5 py-2 pr-3 pl-2 text-[14px] font-[600] hover:rounded-[8px] focus:rounded-[8px]"
+                  >
+                    {page.pageTitle}
+                  </Link>
+                ))}
+              </div>
+
+              {/* 공유페이지에 따른 폴더 생성  */}
+              <div className="mt-4 flex items-center px-[8px] py-[4px] text-[14px] font-[500] text-gray-50 hover:rounded-[8px] focus:rounded-[8px]">
+                <div className="group flex w-full items-center justify-between">
+                  <div className="flex gap-[20px]">
+                    <div>폴더</div>
+                  </div>
+                  <PlusIcon
+                    className="text-gray-40 hover:text-gray-90 cursor-pointer"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      e.preventDefault();
+                      handleCreateFolder();
+                    }}
+                  />
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
-        <div className="p-3">
-          <BookMark
-            width={20}
-            height={20}
-            className="text-gray-70 group-focus:text-primary-50 my-[2px]"
-          />
-        </div>
-      </div>
-    </aside>
+      </aside>
+    )
   );
 };
 
 export default SideBar;
+
+// isFoldSiderbar && (
+//   // 반응형 (사이드바 접힘) 화면
+//   <aside className="border-gray-10 h-screen w-[80px] border-r p-4">
+//     <div className="flex justify-end">
+//       <button
+//         onClick={() => setShowSidebar(true)}
+//         className="mb-2 cursor-pointer"
+//       >
+//         <SidebarOpen />
+//       </button>
+//     </div>
+
+//     <div className="flex flex-col items-center gap-[8px]">
+//       <div className="p-3">
+//         <PersonalPage
+//           width={20}
+//           height={20}
+//           className="group-focus:text-primary-50 text-gray-70"
+//         />
+//       </div>
+//       <div className="p-3">
+//         <BookMark
+//           width={20}
+//           height={20}
+//           className="text-gray-70 group-focus:text-primary-50 my-[2px]"
+//         />
+//       </div>
+//     </div>
+//   </aside>
+// );
