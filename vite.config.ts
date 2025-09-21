a7f70b816c97a1904c816c6ac93fbef4b4a6cc4e
hoonshi <qkekxovnd@hufs-gsuite.kr>
2025-09-22 03:31:46 +0900

    feat: vite에 extension관련 옵션들 제거

--- vite.config.ts
+++ vite.config.ts
@@ -1,50 +1,16 @@
-import path, { resolve } from 'path';
+import path from 'path';
 import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-import { copyFileSync, readFileSync, writeFileSync } from 'fs';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-    svgr(),
-    {
-      name: 'copy-extension-files',
-      async writeBundle() {
-        // manifest.json 복사
-        copyFileSync('src/extension/manifest.json', 'dist/manifest.json');
-
-        // content script 빌드
-        const contentScript = readFileSync('src/extension/content.ts', 'utf8');
-        writeFileSync('dist/content.js', `// Content script\n${contentScript}`);
-
-        // background script 빌드
-        const backgroundScript = readFileSync(
-          'src/extension/background.ts',
-          'utf8'
-        );
-        writeFileSync(
-          'dist/background.js',
-          `// Background script\n${backgroundScript}`
-        );
-      },
-    },
-  ],
+  plugins: [react(), tailwindcss(), svgr()],
   build: {
     rollupOptions: {
-      input: {
-        web: resolve(__dirname, 'index.html'),
-        extension: resolve(__dirname, 'src/extension/main-extension.tsx'),
-      },
       output: {
-        //파일 이름 지정
         entryFileNames: 'assets/[name].[hash].js',
-        //청크 파일 이름 지정
         chunkFileNames: 'assets/chunks/[name].[hash].js',
-        // 에셋 파일 분류
         assetFileNames: (assetInfo) => {
           if (assetInfo.name?.endsWith('.css')) {
             return 'assets/css/[name].[hash].css';
@@ -57,7 +23,6 @@ export default defineConfig({
           }
           return 'assets/[name].[hash][extname]';
         },
-        //성능 개선 유도 manualChunks
         manualChunks: {
           vendor: [
             'react',
@@ -68,8 +33,6 @@ export default defineConfig({
         },
       },
     },
-    outDir: 'dist',
-    assetsDir: 'assets',
     minify: 'terser',
     terserOptions: {
       compress: {
@@ -81,16 +44,10 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
-      '@components': resolve(__dirname, 'src/components'),
-      '@assets': resolve(__dirname, 'src/assets'),
+      '@components': path.resolve(__dirname, 'src/components'),
+      '@assets': path.resolve(__dirname, 'src/assets'),
     },
   },
-  assetsInclude: ['**/*.webp'],
-
-  optimizeDeps: {
-    exclude: ['@emotion/react', '@emotion/styled', '@mui/material'],
-  },
-
   server: {
     proxy: {
       '/api': {
