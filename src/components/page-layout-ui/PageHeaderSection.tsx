408bc0c5e7a64294fd6c8cbad960e898771f66f3
hoonshi <qkekxovnd@hufs-gsuite.kr>
2025-09-30 20:04:37 +0900

    refact: 페이지 헤더 섹션 타이틀 업데이트 훅 사용

--- src/components/page-layout-ui/PageHeaderSection.tsx
+++ src/components/page-layout-ui/PageHeaderSection.tsx
@@ -1,18 +1,13 @@
-import { useState, useEffect, useRef } from 'react';
-import { usePageStore } from '@/stores/pageStore';
-import useUpdateFolder from '@/hooks/mutations/useUpdateFolder';
-import { useDebounce } from '@/hooks/useDebounce';
+import { useState } from 'react';
+
 import { Button } from '../common-ui/button';
 import { useModalStore } from '@/stores/modalStore';
 import { useLocation } from 'react-router-dom';
+import { useUpdateTitle } from '@/hooks/useUpdateTitle';
 
 type PageHeaderSectionProps = {
   pageTitle: string;
   folderId?: string;
-};
-
-type FolderUpdateData = {
-  title: string;
 };
 
 const MAX_TITLE_LENGTH = 12;
@@ -22,62 +17,11 @@ export default function PageHeaderSection({
   folderId,
 }: PageHeaderSectionProps) {
   const [title, setTitle] = useState(pageTitle ?? '');
-  const [isFocused, setIsFocused] = useState<'title' | null>(null);
-  const lastUpdateRef = useRef<FolderUpdateData>({ title });
+  const { debouncedUpdate, handleBlur } = useUpdateTitle(folderId, title);
   const { openLinkModal } = useModalStore();
   const location = useLocation();
   const currentLocation = location.pathname;
   const isLinkButtonVisible = currentLocation !== '/bookmarks';
-
-  const { pageId } = usePageStore();
-  const { mutate: updateFolder } = useUpdateFolder(pageId);
-
-  const updateFolderImmediately = () => {
-    if (!folderId) return;
-
-    const updateData = {
-      baseRequest: { pageId: pageId as string, commandType: 'EDIT' },
-      folderId,
-      folderName: title,
-    };
-
-    updateFolder(updateData, {
-      onSuccess: () => {
-        lastUpdateRef.current = { title };
-      },
-      onError: (error) => {
-        console.error('폴더 업데이트 실패:', error);
-      },
-    });
-  };
-
-  const handleDebouncedUpdate = () => {
-    lastUpdateRef.current = { title };
-  };
-
-  const debouncedUpdate = useDebounce<FolderUpdateData>(
-    handleDebouncedUpdate,
-    500
-  );
-
-  useEffect(() => {
-    setTitle(pageTitle ?? '');
-    const newState = {
-      title: pageTitle ?? '',
-    };
-    lastUpdateRef.current = newState;
-  }, [pageTitle]);
-
-  const handleBlur = () => {
-    const currentPath = window.location.pathname;
-    if (currentPath === '/' || currentPath === '/bookmarks') {
-      return;
-    }
-
-    const currentState = { title };
-    lastUpdateRef.current = currentState;
-    updateFolderImmediately();
-  };
 
   return (
     <div className="mb-[24px] flex w-full min-w-[328px] items-center justify-between">
@@ -93,18 +37,10 @@ export default function PageHeaderSection({
               debouncedUpdate({ title: value });
             }
           }}
-          onFocus={() => {
-            console.log('title input focus');
-            setIsFocused('title');
+          onBlur={() => {
+            handleBlur(title);
           }}
-          onBlur={(e) => {
-            console.log('title input blur', e.target.value);
-            setIsFocused(null);
-            handleBlur();
-          }}
-          className={`inline-block text-[22px] font-bold outline-none ${
-            isFocused === 'title' ? 'text-gray-100' : 'text-gray-90'
-          }`}
+          className={`outline-nontext-gray-90' } inline-block text-[22px] font-bold`}
         />
       </div>
       <div className="hidden md:block">
